e07e54c929fceb60db98e57a86097fee8d6f077e
devarogundade <devarogundade@gmail.com>
2026-04-16 09:46:20 +0100

    Update @railbeam/beam-ts to version 3.0.4 and adjust API endpoint URL
    - Bumped @railbeam/beam-ts version to 3.0.4 in package.json files for improved functionality.
    - Updated the API endpoint URL in endpoints.ts to version 2.0.1 for enhanced data retrieval.

--- sdk/src/utils/endpoints.ts
+++ sdk/src/utils/endpoints.ts
@@ -2,7 +2,7 @@
 export const Endpoints = {
   BASE_GRAPH_URL: {
     Mainnet:
-      "https://api.goldsky.com/api/public/project_cly6563u2bhfy01zw7mah9nhs/subgraphs/railbeam/2.0.0/gn",
+      "https://api.goldsky.com/api/public/project_cly6563u2bhfy01zw7mah9nhs/subgraphs/railbeam/2.0.1/gn",
   },
   BASE_TRANSACTION_URL: {
     Mainnet: "http://localhost:5174",
